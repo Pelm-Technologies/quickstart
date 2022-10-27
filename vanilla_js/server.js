766f491bd0cbba811a9cbf9f80bc4ec8206695ca
Edmund Tian <edmundtian@ezt.local>
2022-10-27 11:55:21 -0400

    wip

--- vanilla_js/server.js
+++ vanilla_js/server.js
@@ -29,82 +29,141 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Display the inde.html at the servers localhost location
+// Serve index.html
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+const baseHeaders = {
+  'Pelm-Client-Id': process.env.PELM_CLIENT_ID,
+  'Pelm-Secret': process.env.PELM_SECRET
+}
+
 // Create connect_token
 // API reference: https://docs.pelm.com/reference/post_auth-connect-token
-app.post('/connect-token', (req, res) => {
+app.post('/connect-token', async (req, res) => {
 
-  const headers = new Headers();
-  headers.set('Pelm-Client-Id', PELM_CLIENT_ID);
-  headers.set('Pelm-Secret', PELM_SECRET);
-
+  const headers = new Headers(baseHeaders);
   const encodedParams = new URLSearchParams();
-
-  encodedParams.set('user_id', USER_ID);
-
+  encodedParams.set('user_id', process.env.USER_ID);
   const requestOptions = {
     method: 'POST',
     headers,
     body: encodedParams,
   };
 
-  fetch('https://api.pelm.com/auth/connect-token', requestOptions)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return response.text().then(text => { throw new Error(text) })
-      }
+  const response = await fetch('https://api.pelm.com/auth/connect-token', requestOptions);
+  const data = await response.json()
+
+  if (response.ok) {
+    console.log("response ok")
+    res.json({
+      'connect_token': data['connect_token']
     })
-    .then((data) => {
-      // Return connect token
-      res.end(JSON.stringify({
-        connectToken: data['connect_token']
-      }))
+  } else {
+    console.log("response not ok")
+    res.status(500).send(data)
+  }
+})
+
+// Create access_token
+// API reference: https://docs.pelm.com/reference/post_auth-token-1
+app.post('/authorization', async (req, res) => {
+
+  const headers = new Headers(baseHeaders);
+  const encodedParams = new URLSearchParams();
+  encodedParams.set('code', req.body.authorization_code);
+  const requestOptions = {
+    method: 'POST',
+    headers,
+    body: encodedParams,
+  };
+
+  const response = await fetch('https://api.pelm.com/auth/token', requestOptions);
+  const data = await response.json()
+
+  console.log("response")
+  console.log(response)
+  console.log(data)
+
+  if (response.ok) {
+    // We recommend securely saving your access_token to your database. We're saving to session here for convenience.
+    req.session.access_token = data.access_token
+    // Use the access_token to make requests for a given User's energy data
+    res.json({
+      'is_successful': true
     })
-    .catch((err) => {
-      res.status(500).send(JSON.parse(err.message).message)
-    });
+  } else {
+    res.status(500).send(data)
+  }
+
+  // await fetch('https://api.pelm.com/auth/token', requestOptions)
+  //   .then((response) => {
+  //     if (response.ok) {
+  //       return response.json()
+  //     } else {
+  //       return response.text().then((text) => {
+  //         throw new Error(text);
+  //       });
+  //     }
+  //   })
+  //   .then(data => {
+  //     // We recommend securely saving your access_token to your database. We're saving to session here for convenience.
+  //     req.session.access_token = data.access_token
+  //     // Use the access_token to make requests for a given user's energy data
+  //     res.json(true)
+  //   })
+  //   .catch((error) => {
+  //     res.status(500).send(JSON.parse(err.message).message)
+  //   });
 })
 
 
 // Get energy_accouts
 // API reference: https://docs.pelm.com/reference/get_accounts
-app.post('/accounts', (req, res) => {
+app.post('/accounts', async (req, res) => {
 
   const accessToken = req.session.access_token
-
-  const headers = new Headers();
+  const headers = new Headers(baseHeaders);
   headers.set('Authorization', 'Bearer ' + accessToken);
-  headers.set('Pelm-Client-Id', PELM_CLIENT_ID);
-  headers.set('Pelm-Secret', PELM_SECRET);
 
   const requestOptions = {
     method: 'GET',
     headers,
   };
 
+  const response = await fetch('https://api.pelm.com/accounts', requestOptions);
+  const data = await response.json()
+
+  console.log("response")
+  console.log(response)
+  console.log(data)
+
+  if (response.ok) {
+    console.log("response ok")
+    res.send(data)
+  } else {
+    console.log("response not ok")
+    res.status(500).send(data)
+  }
+
   fetch('https://api.pelm.com/accounts', requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return response.text().then((text) => {
-          throw new Error(text);
-        });
-      }
-    })
-    .then((data) => {
-      // Return accounts data
-      res.end(JSON.stringify(data))
-    })
-    .catch((error) => {
-      res.status(500).send(JSON.parse(err.message).message)
-    });
+    // .then((response) => {
+    //   if (response.ok) {
+    //     return response.json();
+    //   } else {
+    //     return response.text().then((text) => {
+    //       throw new Error(text);
+    //     });
+    //   }
+    // })
+    // .then((data) => {
+    //   // Return accounts data
+    //   res.end(JSON.stringify(data))
+    // })
+    // .catch((error) => {
+    //   res.status(500).send(JSON.parse(err.message).message)
+    // });
 })
 
 
@@ -194,48 +253,6 @@ app.post('/bills', (req, res) => {
       res.status(500).send(JSON.parse(err.message).message)
     });
 })
-
-
-// Create access_token
-// API reference: https://docs.pelm.com/reference/post_auth-token-1
-app.post('/authorization', async (req, res) => {
-
-  const headers = new Headers();
-  headers.set('Pelm-Client-Id', PELM_CLIENT_ID);
-  headers.set('Pelm-Secret', PELM_SECRET);
-
-  const encodedParams = new URLSearchParams();
-  encodedParams.set('code', req.body.authorizationCode);
-
-  const requestOptions = {
-    method: 'POST',
-    headers,
-    body: encodedParams,
-  };
-
-  await fetch('https://api.pelm.com/auth/token', requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        return response.text().then((text) => {
-          throw new Error(text);
-        });
-      }
-    })
-    .then(data => {
-      // We recommend securely saving your access_token to your database. We're saving to session here for convenience.
-      req.session.access_token = data.access_token
-      // Use the access_token to make requests for a given user's energy data
-      res.json(true)
-    })
-    .catch((error) => {
-      res.status(500).send(JSON.parse(err.message).message)
-    });
-})
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
