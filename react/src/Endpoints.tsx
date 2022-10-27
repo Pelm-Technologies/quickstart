36c36424ea1540259fc62b67854015c18c539fed
Edmund Tian <edmund.tian@berkeley.edu>
2022-10-27 16:19:44 -0400

    Code and UI edits (#6)
    * wip
    
    * vanilla_js changes
    
    * everything working
    
    * links
    
    * clean
    
    Co-authored-by: Edmund Tian <edmundtian@ezt.local>

--- react/src/Endpoints.tsx
+++ react/src/Endpoints.tsx
@@ -6,15 +6,14 @@ type Props = {
 };
 
 type State = {
-    accountsData?: string;
-    intervalData?: any;
-    billsData?: any;
+    accountsResponse?: string;
+    intervalsResponse?: any;
+    billsResponse?: any;
     intervalsAccountIdInput: string;
     intervalsStartDate: string;
     intervalsEndDate: string;
     intervalsType: string;
     billsAccountIdInput: string;
-    billIdInput: string;
 };
 
 const Outer = styled.div`
@@ -30,30 +29,27 @@ export class Endpoints extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            accountsData: undefined,
-            intervalData: undefined,
-            billsData: undefined,
+            accountsResponse: undefined,
+            intervalsResponse: undefined,
+            billsResponse: undefined,
             intervalsAccountIdInput: '',
             intervalsStartDate: '',
             intervalsEndDate: '',
             intervalsType: 'ELECTRIC',
             billsAccountIdInput: '',
-            billIdInput: '',
         };
     }
 
-    // Sending request to our backned at the accounts endpoint
-    fetchAccounts = () => {
-        fetch('/accounts', { method: 'POST' })
-            .then((r) => r.json())
-            .then((data) => {
-                this.setState({ accountsData: JSON.stringify(data, null, 2) });
-            });
+    // Fetch Accounts from server
+    fetchAccounts = async () => {
+        const response = await fetch('/accounts', { method: 'POST' })
+        const data = await response.json()
+        this.setState({ accountsResponse: JSON.stringify(data, null, 2) })
     };
 
-    // Sending request to our backned at the intervals endpoint
-    fetchIntervals = () => {
-        fetch('/intervals', {
+    // Fetch Intervals from server
+    fetchIntervals = async () => {
+        const response = await fetch('/intervals', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -65,15 +61,13 @@ export class Endpoints extends React.Component<Props, State> {
                 intervalsEndDate: this.state.intervalsEndDate,
             }),
         })
-            .then((r) => r.json())
-            .then((d) => {
-                this.setState({ intervalData: d });
-            });
+        const data = await response.json()
+        this.setState({ intervalsResponse: JSON.stringify(data, null, 2) })
     };
 
-    // Sending request to our backned at the bills endpoint
-    fetchBills = () => {
-        fetch('/bills', {
+    // Fetch Bills from server
+    fetchBills = async () => {
+        const response = await fetch('/bills', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -82,88 +76,81 @@ export class Endpoints extends React.Component<Props, State> {
                 billsAccountIdInput: this.state.billsAccountIdInput,
             }),
         })
-            .then((r) => r.json())
-            .then((d) => {
-                this.setState({ billsData: d });
-            });
+        const data = await response.json()
+        this.setState({ billsResponse: JSON.stringify(data, null, 2) })
     };
 
-    // - - - - - - - - - - - - Front-End Rendering Logic - - - - - - - - - - - -
+    // - - - - - - - - - - - - Rendering Logic - - - - - - - - - - - -
 
-    maybeRenderAccountsResponse() {
-        if (this.state.accountsData === undefined) {
-            return null;
-        }
-
-        const data = this.state.accountsData;
-
-        return (
-            <div>
-                This is the response:
-                <div style={{border: '1px solid black', maxHeight: "500px", overflowY: 'scroll' }}>
-                    <pre>{data}</pre>
-                </div>
-            </div>
-        );
-    }
-
+    // Accounts
     renderAccountsEndpoint() {
-        const clearAcountsData = () => {
-            this.setState({ accountsData: undefined });
+        const clearAccountsData = () => {
+            this.setState({ accountsResponse: undefined });
         };
         return (
             <div>
                 <div>
-                    Click this button to make a GET request to <code>/accounts</code>
+                    Click "Submit" to make a request to <code><a href='https://docs.pelm.com/reference/get_accounts' target='_blank'>GET /accounts</a></code>
                 </div>
                 <button onClick={this.fetchAccounts}>Submit</button>
-                {this.state.accountsData && <button onClick={clearAcountsData}>Clear Data</button>}
+                {this.state.accountsResponse && <button onClick={clearAccountsData}>Clear Data</button>}
                 {this.maybeRenderAccountsResponse()}
             </div>
         );
     }
 
-    maybeRenderIntervalsResponse() {
-        if (this.state.intervalData === undefined) {
+    maybeRenderAccountsResponse() {
+        if (this.state.accountsResponse === undefined) {
             return null;
         }
-
-        const clearIntervalData = () => {
-            this.setState({ intervalData: undefined });
-        };
-
         return (
-            <>
-                <button onClick={clearIntervalData}>Clear</button>
-                <div>
-                    This is the response:
-                    <div style={{ border: '1px solid black', maxHeight: "500px", overflowY: 'scroll'  }}>
-                        <pre>{JSON.stringify(this.state.intervalData, null, 2)}</pre>
-                    </div>
+            <div>
+                This is the response:
+                <div style={{border: '1px solid black', maxHeight: "500px", overflowY: 'scroll' }}>
+                    <pre>{this.state.accountsResponse}</pre>
                 </div>
-            </>
+            </div>
         );
     }
 
-    maybeRenderAccountBillsResponse() {
-        if (this.state.billsData === undefined) {
-            return null;
-        }
-
-        const clearBillsData = () => {
-            this.setState({ billsData: undefined });
-        };
-
+    // Intervals
+    renderIntervalsEndpoints() {
         return (
-            <>
-                <button onClick={clearBillsData}>Clear</button>
+            <div>
                 <div>
-                    This is the response:
-                    <div style={{ border: '1px solid black', maxHeight: "500px", overflowY: 'scroll' }}>
-                        <pre>{JSON.stringify(this.state.billsData, null, 2)}</pre>
-                    </div>
+                Click "Submit" to make a request to <code><a href='https://docs.pelm.com/reference/get_intervals' target='_blank'>GET /intervals</a></code>
                 </div>
-            </>
+                <input
+                    id="accountId"
+                    name="accoundId"
+                    type="text"
+                    value={this.state.intervalsAccountIdInput}
+                    onChange={this.onIntervalsInputChange}
+                    placeholder="Enter Account Id"
+                />
+                <input
+                    id="startDate"
+                    name="startDate"
+                    type="text"
+                    value={this.state.intervalsStartDate}
+                    onChange={this.onIntervalsStartDateChange}
+                    placeholder="Enter Start Date"
+                />
+                <input
+                    id="endDate"
+                    name="endDate"
+                    type="text"
+                    value={this.state.intervalsEndDate}
+                    onChange={this.onIntervalsEndDateChange}
+                    placeholder="Enter End Date"
+                />
+                <select onChange={this.onIntervalsTypeChange}>
+                    <option value="ELECTRIC">ELECTRIC</option>
+                    <option value="GAS">GAS</option>
+                </select>
+                <button onClick={this.fetchIntervals}>Submit</button>
+                {this.maybeRenderIntervalsResponse()}
+            </div>
         );
     }
 
@@ -203,60 +190,34 @@ export class Endpoints extends React.Component<Props, State> {
         });
     };
 
-    renderIntervalsEndpoints() {
+    maybeRenderIntervalsResponse() {
+        if (this.state.intervalsResponse === undefined) {
+            return null;
+        }
+
+        const clearIntervalsData = () => {
+            this.setState({ intervalsResponse: undefined });
+        };
+
         return (
-            <div>
+            <>
+                <button onClick={clearIntervalsData}>Clear</button>
                 <div>
-                    Click this button to make a GET request to <code>/intervals</code>
+                    This is the response:
+                    <div style={{ border: '1px solid black', maxHeight: "500px", overflowY: 'scroll'  }}>
+                        <pre>{this.state.intervalsResponse}</pre>
+                    </div>
                 </div>
-                <input
-                    id="accountId"
-                    name="accoundId"
-                    type="text"
-                    value={this.state.intervalsAccountIdInput}
-                    onChange={this.onIntervalsInputChange}
-                    placeholder="Enter Account Id"
-                />
-                <input
-                    id="startDate"
-                    name="startDate"
-                    type="text"
-                    value={this.state.intervalsStartDate}
-                    onChange={this.onIntervalsStartDateChange}
-                    placeholder="Enter Start Date"
-                />
-                <input
-                    id="endDate"
-                    name="endDate"
-                    type="text"
-                    value={this.state.intervalsEndDate}
-                    onChange={this.onIntervalsEndDateChange}
-                    placeholder="Enter End Date"
-                />
-                <select onChange={this.onIntervalsTypeChange}>
-                    <option value="ELECTRIC">ELECTRIC</option>
-                    <option value="GAS">GAS</option>
-                </select>
-                <button onClick={this.fetchIntervals}>Submit</button>
-                {this.maybeRenderIntervalsResponse()}
-            </div>
+            </>
         );
     }
 
-    onBillsInputChange = (event: { target: any }) => {
-        const target = event.target;
-        const value = target.value;
-
-        this.setState({
-            billsAccountIdInput: value,
-        });
-    };
-
+    // Bills
     renderAccountBillsEndpoint() {
         return (
             <div>
                 <div>
-                    Click this button to make a GET request to <code>/bills</code>
+                Click "Submit" to make a request to <code><a href='https://docs.pelm.com/reference/get_bills' target='_blank'>GET /bills</a></code>
                 </div>
                 <input
                     id="accountId"
@@ -272,14 +233,36 @@ export class Endpoints extends React.Component<Props, State> {
         );
     }
 
-    onBillIdInputChange = (event: { target: any }) => {
+    onBillsInputChange = (event: { target: any }) => {
         const target = event.target;
         const value = target.value;
 
         this.setState({
-            billIdInput: value,
+            billsAccountIdInput: value,
         });
     };
+
+    maybeRenderAccountBillsResponse() {
+        if (this.state.billsResponse === undefined) {
+            return null;
+        }
+
+        const clearBillsData = () => {
+            this.setState({ billsResponse: undefined });
+        };
+
+        return (
+            <>
+                <button onClick={clearBillsData}>Clear</button>
+                <div>
+                    This is the response:
+                    <div style={{ border: '1px solid black', maxHeight: "500px", overflowY: 'scroll' }}>
+                        <pre>{this.state.billsResponse}</pre>
+                    </div>
+                </div>
+            </>
+        );
+    }
 
     render() {
         return (
--- vanilla_js/server.js
+++ vanilla_js/server.js
@@ -13,12 +13,6 @@ dotenv.config()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Enviornment variables stored in the .env file you created
-const PELM_CLIENT_ID = process.env.PELM_CLIENT_ID
-const PELM_SECRET = process.env.PELM_SECRET
-const USER_ID = process.env.USER_ID
-
-
 app.use(
   // We use session in lieu of a database in this example for convenience.
   // Instead use your own database to store values like the Authorization_Token
@@ -29,214 +23,86 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Display the inde.html at the servers localhost location
+// Serve index.html
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+const baseHeaders = {
+  'Pelm-Client-Id': process.env.PELM_CLIENT_ID,
+  'Pelm-Secret': process.env.PELM_SECRET
+}
+
 // Create connect_token
 // API reference: https://docs.pelm.com/reference/post_auth-connect-token
-app.post('/connect-token', (req, res) => {
-
-  const headers = new Headers();
-  headers.set('Pelm-Client-Id', PELM_CLIENT_ID);
-  headers.set('Pelm-Secret', PELM_SECRET);
-
+app.post('/connect-token', async (req, res) => {
+  const headers = new Headers(baseHeaders);
   const encodedParams = new URLSearchParams();
-
-  encodedParams.set('user_id', USER_ID);
-
+  encodedParams.set('user_id', process.env.USER_ID);
   const requestOptions = {
     method: 'POST',
     headers,
     body: encodedParams,
   };
 
-  fetch('https://api.pelm.com/auth/connect-token', requestOptions)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return response.text().then(text => { throw new Error(text) })
-      }
+  const response = await fetch('https://api.pelm.com/auth/connect-token', requestOptions);
+  const data = await response.json()
+  if (response.ok) {
+    res.json({
+      'connect_token': data['connect_token']
     })
-    .then((data) => {
-      // Return connect token
-      res.end(JSON.stringify({
-        connectToken: data['connect_token']
-      }))
-    })
-    .catch((err) => {
-      res.status(500).send(JSON.parse(err.message).message)
-    });
+  } else {
+    res.status(500).send(data)
+  }
 })
-
-
-// Get energy_accouts
-// API reference: https://docs.pelm.com/reference/get_accounts
-app.post('/accounts', (req, res) => {
-
-  const accessToken = req.session.access_token
-
-  const headers = new Headers();
-  headers.set('Authorization', 'Bearer ' + accessToken);
-  headers.set('Pelm-Client-Id', PELM_CLIENT_ID);
-  headers.set('Pelm-Secret', PELM_SECRET);
-
-  const requestOptions = {
-    method: 'GET',
-    headers,
-  };
-
-  fetch('https://api.pelm.com/accounts', requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return response.text().then((text) => {
-          throw new Error(text);
-        });
-      }
-    })
-    .then((data) => {
-      // Return accounts data
-      res.end(JSON.stringify(data))
-    })
-    .catch((error) => {
-      res.status(500).send(JSON.parse(err.message).message)
-    });
-})
-
-
-// Get energy usage intervals
-// API reference: https://docs.pelm.com/reference/get_intervals
-//
-// Not currently being utilized in this example implementation
-// Look at our React implementation for more information
-app.post('/intervals', (req, res) => {
-
-  const accessToken = req.session.access_token
-  const headers = new Headers();
-  headers.set('Authorization', 'Bearer ' + accessToken);
-  headers.set('Pelm-Client-Id', PELM_CLIENT_ID);
-  headers.set('Pelm-Secret', PELM_SECRET);
-
-  const requestOptions = {
-    method: 'GET',
-    headers,
-  };
-
-  const url =
-    'https://api.pelm.com/intervals?' +
-    new URLSearchParams({
-      account_id: req.body.intervalsAccountIdInput,
-      type: req.body.intervalsType,
-      start_date: req.body.intervalsStartDate,
-      end_date: req.body.intervalsEndDate,
-    });
-
-  fetch(url, requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return response.text().then((text) => {
-          throw new Error(text);
-        });
-      }
-    })
-    .then((data) => {
-      // Return intervals data
-      res.end(JSON.stringify(data));
-    })
-    .catch((error) => {
-      res.status(500).send(JSON.parse(err.message).message)
-    });
-
-})
-
-
-// Get Bills
-// API reference: https://docs.pelm.com/reference/get_bills
-//
-// Not currently being utilized in this example implementation
-// Look at our React implementation for more information
-app.post('/bills', (req, res) => {
-
-  const accessToken = req.session.access_token
-  const headers = new Headers();
-  headers.set('Authorization', 'Bearer ' + accessToken);
-  headers.set('Pelm-Client-Id', PELM_CLIENT_ID);
-  headers.set('Pelm-Secret', PELM_SECRET);
-
-  const requestOptions = {
-    method: 'GET',
-    headers,
-  };
-
-  const url = 'https://api.pelm.com/bills?account_id=' + req.body.billsAccountIdInput;
-
-  fetch(url, requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return response.text().then((text) => {
-          throw new Error(text);
-        });
-      }
-    })
-    .then((data) => {
-      // Return bills data
-      res.end(JSON.stringify(data));
-    })
-    .catch((error) => {
-      res.status(500).send(JSON.parse(err.message).message)
-    });
-})
-
 
 // Create access_token
 // API reference: https://docs.pelm.com/reference/post_auth-token-1
 app.post('/authorization', async (req, res) => {
-
-  const headers = new Headers();
-  headers.set('Pelm-Client-Id', PELM_CLIENT_ID);
-  headers.set('Pelm-Secret', PELM_SECRET);
-
+  const headers = new Headers(baseHeaders);
   const encodedParams = new URLSearchParams();
-  encodedParams.set('code', req.body.authorizationCode);
-
+  encodedParams.set('code', req.body.authorization_code);
   const requestOptions = {
     method: 'POST',
     headers,
     body: encodedParams,
   };
 
-  await fetch('https://api.pelm.com/auth/token', requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        return response.text().then((text) => {
-          throw new Error(text);
-        });
-      }
+  const response = await fetch('https://api.pelm.com/auth/token', requestOptions);
+  const data = await response.json()
+  if (response.ok) {
+    // We recommend securely saving your access_token to your database. We're saving to session here for convenience.
+    req.session.access_token = data.access_token
+    // Use the access_token to make requests for a given User's energy data
+    res.json({
+      'is_successful': true
     })
-    .then(data => {
-      // We recommend securely saving your access_token to your database. We're saving to session here for convenience.
-      req.session.access_token = data.access_token
-      // Use the access_token to make requests for a given user's energy data
-      res.json(true)
-    })
-    .catch((error) => {
-      res.status(500).send(JSON.parse(err.message).message)
-    });
+  } else {
+    res.status(500).send(data)
+  }
 })
 
+// Get Accounts
+// API reference: https://docs.pelm.com/reference/get_accounts
+app.post('/accounts', async (req, res) => {
+  const accessToken = req.session.access_token
+  const headers = new Headers(baseHeaders);
+  headers.set('Authorization', 'Bearer ' + accessToken);
 
+  const requestOptions = {
+    method: 'GET',
+    headers,
+  };
 
+  const response = await fetch('https://api.pelm.com/accounts', requestOptions);
+  const data = await response.json()
+  if (response.ok) {
+    res.send(data)
+  } else {
+    res.status(500).send(data)
+  }
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server listening on port ${port}`)
 })
